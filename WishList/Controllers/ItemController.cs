4a2ed803d9be5ac5e0ca49d934b67c7b0898fb7d
Muhammad Khokharawan <Muhammad.Khokharawan@frasers.group>
2022-06-06 17:14:06 +0100

    Item Controller implemented

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return Index();
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            _context.Remove(Id);
+            _context.SaveChanges();
+            return Index();
+        }
+    }
+}
