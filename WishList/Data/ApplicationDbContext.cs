e529f4f3dc199a720ca11962ea3924a7da73a4f8
Muhammad Khokharawan <Muhammad.Khokharawan@frasers.group>
2022-06-06 16:35:38 +0100

    Models created and services configured for Database

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items;
+        // Invokes base constructor as well
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
